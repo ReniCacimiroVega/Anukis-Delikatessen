b8a550c73f8a046a79bb5a449bad7417f91b411b
ReniCacimiroVega <79757907+ReniCacimiroVega@users.noreply.github.com>
2021-05-19 19:56:49 -0300

    archivo js correcto de shop

--- js/app-shop.js
+++ js/app-shop.js
@@ -1,3 +1,5 @@
+/** S: Animando la pagina ***********************************/
+
 let animacionScroll = document.querySelectorAll(".ScrollFade");
 
 function ScrollFade (){
@@ -10,5 +12,59 @@ function ScrollFade (){
 	}
 }
 
-
 window.addEventListener('scroll', ScrollFade)
+
+/** S: Carrito **********************************************/
+
+function leer_carrito() {
+  var carrito = JSON.parse(localStorage.getItem('carrito')); //U: con parse se vuelve un elemento legible
+  if (!carrito) { //U: La primera vez carga nulo, lo seteo
+		carrito = [
+			[0, 0, 0, 0], //A: Carne y remolacha
+			[0, 0, 0, 0], //A: Pollo y oregano
+			[0, 0, 0, 0], //A: Higado de pollo
+			//A: Mini, Chico, Mediano, Grande
+		];
+	} 
+
+	return carrito;
+}
+
+function modificar_carrito(tipo, tamanio, cant) {
+	var carrito = leer_carrito();
+
+	if ((carrito[tipo][tamanio] + cant) >= 0) { //A: Reviso que la cantidad "tenga sentido" (sea un numero positivo o cero)
+		carrito[tipo][tamanio] += cant;
+	} else {
+		carrito[tipo][tamanio] = 0; //A: 0 de piso
+	}
+
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+
+	actualizar_texto_carrito(tipo, tamanio, carrito[tipo][tamanio]);
+}
+
+function actualizar_texto_carrito(tipo, tamanio, cant) {
+	document.getElementById('carrito_' + tipo + '_' + tamanio).innerText = "Cantidad: " + cant;
+}
+
+function calcular_precio_carrito() {
+	var carrito = leer_carrito();
+	var total = 0;
+
+	var precios = [
+		[250, 250, 220, 210], //A: Carne y remolacha
+		[230, 230, 200, 190], //A: Pollo y oregano
+		[220, 220, 190, 180], //A: Higado de pollo
+		//A: Mini, Chico, Mediano, Grande
+	];
+
+	//A: Itero sobre carrito y precios, multiplicando cada posicion respectivamente
+	for (i in carrito) { //A: Cada fila
+		for (j in carrito[i]) { //A: Cada columna
+			total += (carrito[i][j] * precios[i][j]);
+		}
+	}
+	
+	$("#recibo").append( "<p style='font-size: 20px; color: white; background-color: black;' >El total a pagar de su orden es: </p>" + total);
+} 
