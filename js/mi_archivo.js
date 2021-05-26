03d6816137662b7cc45ce6f3a8d7b87a91d9d238
ReniCacimiroVega <79757907+ReniCacimiroVega@users.noreply.github.com>
2021-05-26 18:09:34 -0300

    validar form comentarios (no funciona)

--- js/mi_archivo.js
+++ js/mi_archivo.js
@@ -1,6 +1,5 @@
 /** LIBRERIA ***********************/
 
-
 function guardar_aplicacion() { //U: Guarda una aplicacion
 	var aplicaciones = JSON.parse(localStorage.getItem('aplicaciones'))
 	
@@ -40,7 +39,57 @@ function guardar_aplicacion() { //U: Guarda una aplicacion
 
 
 
-function guardar_comentario() { //U: Guarda un comentario 
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+	texto: /^[a-zA-ZÀ-ÿ0-9._-\s?,]{30,360}/ //letras, numeros, guiones, guion bajo, 
+											//puntos, acentos, mayus, espacios, min 30 y max 360
+}
+
+const campos_form2 = {
+	nombre: false,
+	email: false,
+	texto: false
+}
+
+const validarForm2 = (e) =>{
+	switch(e.target.name){
+		case "nombre":
+			validarCampoForm2(expresiones.nombre, e.target, "nombre")
+		break;
+
+		case "email":
+			validarCampoForm2(expresiones.email, e.target, "email")
+		break;
+
+		case "texto":
+			validarCampoForm2(expresiones.textp, e.target, "texto")
+		break;
+	}
+}
+
+function validarCampoForm2(expresion, input, campo){
+	if(expresion.test(input.value)){
+		campos_form2[campo] = true;
+	} else{
+		campos_form2[campo] = false
+	}
+}
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validarForm2());
+	input.addEventListener('blur', validarForm2());
+});
+
+
+function guardar_comentario(e){
+	e.preventDefault();
 	var comentarios = JSON.parse(localStorage.getItem('comentarios')); //U: con parse se vuelve un elemento legigle
 	if (!comentarios) { comentarios = []; } //U: La primera vez carga nulo, lo seteo
 
@@ -49,7 +98,12 @@ function guardar_comentario() { //U: Guarda un comentario
 	comentario.mail = document.getElementById('mailF2').value;	
 	comentario.texto = document.getElementById('textoF2').value;
 
-	if ((comentario.nombre == "") || (comentario.mail == "") || (comentario.texto == "")){
+	if (campos_form2.nombre && campos_form2.email && campos_form2.texto){
+		comentarios.push(comentario);
+		localStorage.setItem('comentarios', JSON.stringify(comentarios));
+		alert("se guardo correctamente su comentario");
+
+	} else{
 		$(".form-contacto").append("<p style='background: white' class='resp-form-contact'>Debe completar todos los campos</p>");
 		$(".resp-form-contact").css({
 									"text-align": "center",
@@ -58,15 +112,13 @@ function guardar_comentario() { //U: Guarda un comentario
 									"border-radius": "10px", "margin":"10% 0% 0% 0%",
 									"height": "3em", "width":"100%",
 									"padding-top": "3%",
+									})
 
-		})
 	}
-	else{
-		comentarios.push(comentario);
-		localStorage.setItem('comentarios', JSON.stringify(comentarios));
-	}
-
 }
+
+
+	
 
 
 /* Programa */
