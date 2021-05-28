5d3a00b7f7973e2fb80939c90eba7046ba767d06
ReniCacimiroVega <79757907+ReniCacimiroVega@users.noreply.github.com>
2021-05-28 13:15:32 -0300

    correcciones en las expresiones de validacion

--- js/mi_archivo.js
+++ js/mi_archivo.js
@@ -45,9 +45,9 @@ const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-	texto: /^[a-zA-ZÀ-ÿ0-9._-\s?,]{30,360}/ //letras, numeros, guiones, guion bajo, 
+	texto: /^.{20,360}/ //letras, numeros, guiones, guion bajo, 
 											//puntos, acentos, mayus, espacios, min 30 y max 360
 }
 
